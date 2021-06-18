5ca00ff39b54d97115fb2027115653b31d811999
Kabir Singh Shekhawat <kabirsinghshekhawat1789@gmail.com>
2021-06-19 01:07:55 +0530

    feat: Add Reset token route, controller

--- routes/otp.js
+++ routes/otp.js
@@ -7,9 +7,15 @@ router
     .route('/')
     .get(auth.isLoggedIn, otpController.getOtp)
     .post(auth.isLoggedIn, otpController.sendOtp)
+
 router
     .route('/check')
     .get(auth.isLoggedIn, otpController.getOtpCheck)
     .post(auth.isLoggedIn, otpController.postOtpCheck)
+
+router
+    .route('/reset/:token')
+    .get(otpController.getResetToken)
+    .post(otpController.postResetToken)
 
 module.exports = router
